91bc9e16a215c12418a90d4ce36b287ae306a9fe
Paul De Silva <p.desilva@klab.com>
2014-09-22 15:02:03 +0800

    fixing .gitignore

---
+++ src/Ch1N9.java
@@ -0,0 +1,12 @@
+
+public class Ch1N9 {
+	public static void main(String args[])
+	{
+		double myWeight_kg = 60;
+		double myWeightOnMoon_kg = myWeight_kg * 0.17;
+		
+		
+		System.out.println("My weight on the moon is " + myWeightOnMoon_kg);
+	}
+
+}
