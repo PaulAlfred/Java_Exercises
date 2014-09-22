eee511bfd0f1d7e02b0a766290ea701fdc63e76e
Paul De Silva <p.desilva@klab.com>
2014-09-22 15:04:02 +0800

    Chapter1 #9 code

--- src/Ch1N9.java
+++ src/Ch1N9.java
@@ -5,7 +5,6 @@ public class Ch1N9 {
 		double myWeight_kg = 60;
 		double myWeightOnMoon_kg = myWeight_kg * 0.17;
 		
-		
 		System.out.println("My weight on the moon is " + myWeightOnMoon_kg);
 	}
 
